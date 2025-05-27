9aa7dd22c1b3a9ceaf0e5a789009cb41ce8346f7
yashchalke8975 <yash.chalke.work@gmail.com>
2025-05-28 01:15:07 +0530

    new practical 4

---
+++ Practical_4(Running..)/Practical_4/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practical_4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 450px">
+    <form id="form1" runat="server">
+        <div style="height: 420px">
+            <asp:TextBox ID="Lastname" runat="server" style="z-index: 1; left: 167px; top: 127px; position: absolute; right: 1072px; margin-top: 0px"></asp:TextBox>
+            <asp:TextBox ID="firstname" runat="server" style="z-index: 1; left: 165px; top: 87px; position: absolute"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="z-index: 1; left: 141px; top: 177px; position: absolute" Text="Button" />
+            <asp:Label ID="Label2" runat="server" style="z-index: 1; left: 37px; top: 134px; position: absolute" Text="Last Name"></asp:Label>
+            <asp:Label ID="Label1" runat="server" style="z-index: 1; left: 35px; top: 87px; position: absolute" Text="First Name"></asp:Label>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+    OnSelectedIndexChanged="GridView1_SelectedIndexChanged"
+    style="z-index: 1; left: 35px; top: 243px; position: absolute; height: 152px; width: 232px">
+    <Columns>
+        <asp:BoundField DataField="Firstname" HeaderText="Firstname" SortExpression="Firstname" />
+        <asp:BoundField DataField="Lastname" HeaderText="Lastname" SortExpression="Lastname" />
+    </Columns>
+</asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practical_4(Running..)/Practical_4/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Practical_4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private readonly string connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\chalk\source\repos\Practical_4\Practical_4\App_Data\Database1.mdf;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadData();
+            }
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand("Insert into yash values (@Firstname,@Lastname)",con))
+                {
+                    cmd.Parameters.AddWithValue("@Firstname", firstname.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Lastname", Lastname.Text.Trim());
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+                    firstname.Text = "";
+                    Lastname.Text = "";
+                    LoadData();
+
+
+
+                }
+            }
+
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                using ( SqlDataAdapter da = new SqlDataAdapter("Select Firstname,Lastname from yash",con))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+       
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // No action needed here unless you want to handle selection
+        }
+    }
+}
